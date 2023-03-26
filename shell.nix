47fa0c9f28fbb6df2368ea26a5377f4ee061002d
Patrick Shaikh <patrick.shaikh@mayflower.de>
2023-03-26 15:24:29 +0200

    basic project setup

---
+++ shell.nix
@@ -0,0 +1,14 @@
+let
+    nixgl = import (fetchTarball "https://github.com/guibou/nixGL/archive/master.tar.gz") {
+      inherit pkgs;
+    };
+    pkgs = import (fetchTarball "https://github.com/nixos/nixpkgs/archive/6adf48f53d819a7b6e15672817fa1e78e5f4e84f.tar.gz") {};
+in
+    pkgs.mkShell.override { stdenv = pkgs.clangStdenv; } {
+        buildInputs = [
+            # Tooling
+            pkgs.just
+            pkgs.terraform
+            pkgs.minio-client
+        ];
+    }
